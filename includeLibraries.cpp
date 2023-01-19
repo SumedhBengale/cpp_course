fd3824b2ffb2dc50ebeb475598429c9e8fdfaebb
SumedhBengale <ssbengale@gmail.com>
2023-01-19 13:56:22 +0530

    Import Libraries

---
+++ includeLibraries.cpp
@@ -0,0 +1,19 @@
+#include<iostream> //Normal C++ Inclusion
+
+#include<stdio.h> //C method to import header files
+
+//Add c to the name of the library and remove .h from its name
+#include<time.h> //C format
+#include<ctime> //C++ syntax format to include compatible libraries
+
+//Another Example
+#include<stdlib.h>
+#include<cstdlib>
+
+//Non System Libraries or Imports
+#include "io.cpp"
+// #include "path/to/the/file/file.extension"
+
+
+//Duplicate imports will be ignored by the Compiler
+#include<iostream>
